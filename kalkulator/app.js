afd31e164eba17a3c4c1b9cb29b08c05dc94650a
7starsnow <runar@svenning.co>
2024-09-03 14:08:22 +0200

    Lagt til Minus, Gange, Dele

--- kalkulator/app.js
+++ kalkulator/app.js
@@ -1,6 +1,9 @@
 const txtTall1 = document.querySelector("#Tall1")
 const txtTall2 = document.querySelector("#Tall2")
 const btnPluss = document.querySelector("#Pluss")
+const btnMinus = document.querySelector("#Minus")
+const btnGange = document.querySelector("#Gange")
+const btnDele = document.querySelector("#Dele")
 const svarDiv = document.querySelector("#Div")
 
 btnPluss.onclick = () => {
@@ -12,3 +15,29 @@ btnPluss.onclick = () => {
     svarDiv.innerHTML = sum;
 }
 
+btnMinus.onclick = () => {
+    let Tall1 = Number(txtTall1.value);
+    let Tall2 = Number(txtTall2.value);
+
+    let sum = Tall1 - Tall2
+
+    svarDiv.innerHTML = sum;
+}
+
+btnGange.onclick = () => {
+    let Tall1 = Number(txtTall1.value);
+    let Tall2 = Number(txtTall2.value);
+
+    let sum = Tall1 * Tall2
+
+    svarDiv.innerHTML = sum;
+}
+
+btnDele.onclick = () => {
+    let Tall1 = Number(txtTall1.value);
+    let Tall2 = Number(txtTall2.value);
+
+    let sum = Tall1 / Tall2
+
+    svarDiv.innerHTML = sum;
+}
