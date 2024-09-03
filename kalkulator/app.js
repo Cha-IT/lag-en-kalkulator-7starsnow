29d36590b5ead96f3eae106d9555c388dc0386e6
7starsnow <runar@svenning.co>
2024-09-03 13:55:53 +0200

    Lagt til css

--- kalkulator/app.js
+++ kalkulator/app.js
@@ -1,23 +1,14 @@
-// Først henter vi ut alle elementene vi skal bruke fra HTML
-const txtTall1 = document.querySelector("#tall1")
-const txtTall2 = document.querySelector("#tall2")
-const btnPluss = document.querySelector("#pluss")
-const svarDiv = document.querySelector("#svar")
+const txtTall1 = document.querySelector("#Tall1")
+const txtTall2 = document.querySelector("#Tall2")
+const btnPluss = document.querySelector("#Pluss")
+const svarDiv = document.querySelector("#Div")
 
-btnPluss.onclick = function() {
-    let tall1 = Number(txtTall1.value);
-    let tall2 = Number(txtTall2.value);
-    let sum = tall1 + tall2
-    svarDiv.innerHTML = sum
+btnPluss.onclick = () => {
+    let Tall1 = Number(txtTall1.value);
+    let Tall2 = Number(txtTall2.value);
+
+    let sum = Tall1 + Tall2
+
+    svarDiv.innerHTML = sum;
 }
 
-/*
-    En alternativ måte å skrive en funksjon er å lage en "arrow function", slik:
-        () => { }
-    Dette btyr det samme som å skrive
-        function() { }
-        men det er bare en slags kortere måte å lage en funksjon, f.eks:
-        btnpluss.onclick = () => {
-        ...
-        }
-*/
